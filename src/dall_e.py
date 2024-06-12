7e97224ca6b970659e7af79fc1cf2222ed3a6ce6
Ned Stickler <nedstickler@gmail.com>
2024-06-12 22:45:39 +0100

    DallE class for image generation

--- src/dall_e.py
+++ src/dall_e.py
@@ -1,15 +1,29 @@
 
 import os
 from openai import OpenAI
+from openai.types import ImagesResponse
 from dotenv import load_dotenv
+
+
+class DallE():
+    def __init__(self, api_key: str) -> None:
+        self.api_key = api_key
+        self._client = OpenAI(api_key=api_key)
+
+    def create(self, prompt: str) -> ImagesResponse:
+        response = self._client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1
+        )
+        return response
 
 
 if __name__ == "__main__":
     load_dotenv("..")
     secret_key = os.getenv("OPEN_AI_KEY")
-    client = OpenAI(api_key=secret_key)
-
-    green = "#00FF00"
-
-    response = client.images.generate(model="dall-e-3", prompt=f"a tomato, with a background of a single solid colour, and no shadows cast by the tomato", size="1024x1024", quality="standard", n=1)
+    dalle = DallE(secret_key)
+    response = dalle.create("a tomato")
     print(response)
