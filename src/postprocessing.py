ac661312d4e49050f7af61d1984e7b761f2fc8f1
Ned Stickler <nedstickler@gmail.com>
2024-07-01 15:43:32 +0100

    Postprocessing function

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -1,26 +1,17 @@
 
-import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
 from rembg import remove
 from pathlib import Path
 
 
-def postprocess(image: Path, dimensions: tuple[int, int] = (32, 32), remove_bg: bool = True):
-    if isinstance(img, Path):
-        img = Image.open(image)
-    else:
-        img = image
+def postprocess(image_path: str | Path, save_path: str | Path | None = None, dimensions: tuple[int, int] = (32, 32), remove_bg: bool = True):
+    img = Image.open(image_path)
     if remove_bg:
         img = remove(img)
     img = img.resize(dimensions, resample=Image.Resampling.NEAREST)
-    return img
+    if save_path is None:
+        save_path = image_path[:-4] + "_ppa.png"
+    img.save(save_path)
     
-
-
 if __name__ == "__main__":
-    # img = postprocess(r".\assets\pickaxe.png")
-    # plt.imshow(img)
-    # plt.show()
-    img = Image.open(r".\assets\pickaxe.png")
-    print(type(img))
+    postprocess(r".\assets\pickaxe.png", dimensions=(64, 64))
