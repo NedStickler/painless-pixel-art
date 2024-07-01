0872aabda9e7cb0f9213bcd2fd08b04a116866c3
Ned Stickler <nedstickler@gmail.com>
2024-07-01 23:09:01 +0100

    Refactor Generator class to allow for post-processing param updating

--- src/generate.py
+++ src/generate.py
@@ -8,10 +8,16 @@ from dotenv import load_dotenv
 class Generator:
     def __init__(self, api_key: str | None = None) -> None:
         self.dall_e = DallE(api_key)
+        self.__postprocess_params = {
+            "save_path": None,
+            "dimensions": (32, 32),
+            "remove_bg": True,
+            "keep_original": True
+        }
     
     def generate(self, item: str, save_path: str | Path) -> None:
         self.dall_e.create(
-            f"""Generate a realistic {item}.
+            f"""Generate {item}.
             Do not draw any shadows.
             Do not add any additional frills to the image.
             Ensure the object is completely within the image bounds.
@@ -19,10 +25,16 @@ class Generator:
             """,
             save_path
         )
-        postprocess(save_path, keep_original=False)
+        postprocess(save_path, **self.__postprocess_params)
+    
+    def update_postprocess_params(self, update_dict: dict) -> None:
+        for key in update_dict.keys():
+            if key not in self.__postprocess_params:
+                raise KeyError(f"No param named {key} to update.")
+        self.__postprocess_params.update(update_dict)
 
 
 if __name__ == "__main__":
     load_dotenv("..")
     generator = Generator()
-    generator.generate("dog", r".\assets\test_item.png")
+    generator.generate("a beer glass", r".\assets\test_item.png")
